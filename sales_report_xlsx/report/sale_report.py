e943ee274ef071a55e12c082275b9fd9c61332a7
Anthony Nwobodo <anthonynwobodo7@gmail.com>
2020-06-06 07:34:06 +0100

    Installed Odoo_report_xlsx module and created xlsx report for sales order

--- sales_report_xlsx/report/sale_report.py
+++ sales_report_xlsx/report/sale_report.py
@@ -6,12 +6,26 @@ class saleOrderXlsx(models.AbstractModel):
     _inherit = 'report.odoo_report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, saleOrder):
+        sheet = workbook.add_worksheet('Sales Orders')
+        bold = workbook.add_format({'bold': True})
+        sheet.write(0, 0, 'Quotation name', bold)
+        sheet.write(0, 1, 'Order Date', bold)
+        sheet.write(0, 2, 'Products', bold)
+        sheet.write(0, 3, 'Customer', bold)
+        sheet.write(0, 4, 'Sales Person', bold)
+        sheet.write(0, 5, 'Company', bold)
+        sheet.write(0, 6, 'Total', bold)
+        line = 2
         for obj in saleOrder:
-            report_name = obj.name
-            # One sheet by partner
-            sheet = workbook.add_worksheet(report_name[:31])
-            bold = workbook.add_format({'bold': True})
-            sheet.write(0, 0, 'Quotation name', bold)
-            sheet.write(2, 0, obj.name)
-            sheet.write(0, 1, 'Order Date', bold)
-            sheet.write(2, 1, obj.date_order)
+            sheet.write(line, 0, obj.name)
+            sheet.write(line, 1, obj.date_order)
+            products = []
+            for order in obj.order_line:
+                products.append(order.product_id.name + ' - ' + str(order.product_uom_qty))
+                sheet.write(line, 2, ", ".join(products))
+            sheet.write(line, 3, obj.partner_id.name)
+            sheet.write(line, 4, obj.user_id.name)
+            sheet.write(line, 5, obj.company_id.name)
+            sheet.write(line, 6, obj.amount_total)
+
+            line = line + 1
